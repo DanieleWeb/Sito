12fb0dbdc1366e6f4d073cf87d936233a978c6dd
RandomGuy <103065933+DanieleWeb@users.noreply.github.com>
2025-06-03 22:07:53 +0300

    Delete main.js

--- main.js
+++
@@ -1,3 +0,0 @@
-function buttonClicked(){
-	window.alert("Benvenuto su questo sito, lettore. In questo momento il sito è in fase di sviluppo e non presenta nulla d'interessante. Grazie per questa visita!")
-}
