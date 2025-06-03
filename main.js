f14d6ea0de4a9f1dbed08b4e7e7e7911b766253d
RandomGuy <103065933+DanieleWeb@users.noreply.github.com>
2025-06-03 22:09:39 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,39 @@
+function buttonClickedInfo(){
+	window.alert("Benvenuto su questo sito, lettore. In questo momento il sito è in fase di sviluppo e non presenta nulla d'interessante. Grazie per questa visita!")
+}
+let i
+function buttonClickedShow(){
+	for(i = 0; i < document.getElementsByClassName("hideButton").length;i++){
+		let x = document.getElementsByClassName("hideButton").item(i);
+		if(x.style.display === "none"){
+			x.style.display = "initial"
+			document.getElementById("show_hide_image").src = "Immagini/hide.svg"; 
+		}
+		else{
+			x.style.display = "none"
+			document.getElementById("show_hide_image").src = "Immagini/show.svg"; 
+		}
+	}
+}
+let isMobile
+if(navigator.maxTouchPoints === 0){
+	isMobile = true
+	console.log(isMobile)
+	test()
+}
+else{
+	isMobile = false
+}
+function buttonSize(){
+	if(navigator.maxTouchPoints > 0){
+		isMobile = true
+		console.log(isMobile)
+		for(i = 0; i < document.getElementsByClassName("navbutton").length; i++){
+			document.getElementsByClassName("navbutton").item(i).style.height = "40px";
+			document.getElementsByClassName("hideButton").item(i).style.height = "40px";
+		}
+	}
+	else{
+		isMobile = false
+	}
+}
